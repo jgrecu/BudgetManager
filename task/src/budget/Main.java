03ed7336827979e8f98a7141d24ac36556940f4d
Jeremy Grecu <jeremy.grecu@sap.com>
2021-12-03 12:43:22 +0000

    JBA - Budget Manager - Stage 1/5: Count my money

--- task/src/budget/Main.java
+++ task/src/budget/Main.java
@@ -1,7 +1,17 @@
 package budget;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        final Scanner sc = new Scanner(System.in);
+        double total = 0.0d;
+        while (sc.hasNextLine()) {
+            String purchase = sc.nextLine();
+            double price = Double.parseDouble(purchase.substring(purchase.lastIndexOf("$") + 1));
+            total += price;
+            System.out.println(purchase);
+        }
+        System.out.printf("%nTotal: $%.2f", total);
     }
 }
